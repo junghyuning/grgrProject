2fb2b61bbe080e2cc09482cc44b6ed3fc6158585
asy98 <asy98@LAPTOP-8VS1EDCJ>
2023-09-24 16:18:16 +0900

    dd

---
+++ grgrmain/src/main/java/com/grgr/exception/CartDeleteFailException.java
@@ -0,0 +1,15 @@
+package com.grgr.exception;
+
+public class CartDeleteFailException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public CartDeleteFailException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartDeleteFailException(String message) {
+		super(message);
+	}
+	
+}
---
+++ grgrmain/src/main/java/com/grgr/exception/CartNullException.java
@@ -0,0 +1,12 @@
+package com.grgr.exception;
+
+public class CartNullException extends Exception{
+	public CartNullException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartNullException(String message) {
+		super(message);
+	}
+	
+}
