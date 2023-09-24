41b364d39543979ed3379e6adc07791cd39f977e
KimJungHyun <bethy0716@gmail.com>
2023-09-25 03:11:08 +0900

    주문목록 출력

--- grgrmain/src/main/java/com/grgr/dto/OrderListDTO.java
+++ grgrmain/src/main/java/com/grgr/dto/OrderListDTO.java
@@ -9,12 +9,13 @@ import lombok.Data;
 public class OrderListDTO {
 	private int orderNo;//주문 테이블 번호(PK)
 	private int orderGroup; //주문번호
-	private int uno;//회원번호
 	private int productId;//상품번호
 	private Date orderDate;//주문 일자
 	private int orderStatus;//주문 상태(1: 결제대기 2: 결제중, 3: 결제완료)
 	private int orderQuantity;//주문 수량
+	private int uno;//판매자의회원번호
 	private String productTitle; // 상품 제목
 	private String productContent; // 상품 설명
 	private int productPrice; // 상품 가격
+	private String nickname; // 판매자 닉네임
 }
